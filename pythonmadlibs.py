9fca52498b65c93704f242cfc7cb24e196acf41b
Ren <rensan961@gmail.com>
2025-09-04 20:53:25 -0500

    Add files via upload

---
+++ pythonmadlibs.py
@@ -0,0 +1,10 @@
+#madlibs game
+adj1 = input("Enter an adjective(description): ")
+noun1 = input("Enter a noun (person, place, or thing): ")
+adj2 = input("Enter an 2nd adjective(description): ")
+adj3 = input("Enter an 3nd adjective(description): ")
+
+print(f"Today I went to a/an {adj1} grocery store.")
+print(f"In the produce section, I saw a/an {noun1}")
+print(f"The {noun1} was {adj2}")
+print(f"I was {adj3}")
